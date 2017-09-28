e4b1c25a732832b952ae43750a9e38d48adaf3b2
Nicole Janosi <janosin@umich.edu>
2017-09-28 10:18:46 -0400

    update

--- 206project1.py
+++ 206project1.py
@@ -1,6 +1,7 @@
 import os
 import filecmp
 import re
+import csv
 
 def getData(file):
 #Input: file name
@@ -94,7 +95,7 @@ def findDay(a):
 
 
 	for k in day_count:
-		if day_count[k] >= max_:
+		if day_count[k] > max_:
 			max_ = day_count[k]
 			day = k
 	
@@ -107,17 +108,65 @@ def findAge(a):
 # Output: Return the day of month (1-31) that is the
 # most often seen in the DOB
 
-	#Your code here:
-	pass
+	list_years = list()
+	lst = list()
+	dates = list()
+	ages = list()
+	
+	for dic in a:
+		dates.append(dic['DOB'])
+
+	for items in dates:
+		years = re.findall('[/][0-9]+[/]([0-9]+)', items)
+		list_years.append(years)
+
+
+	count = 0
+	for i in list_years:
+		for y in i:
+			age = 2017 - int (y)
+			count = count + 1
+			if age < 0:
+				age = -(age)		
+			ages.append(age)
+				
+	q = 0			
+	for x in ages:
+		q = q + x
+	average = q / count
+		
+
+	
+
+
+
+
+
+	return int (average)
 
 #Similar to mySort, but instead of returning single
 #Student, all of the sorted data is saved to a csv file.
 def mySortPrint(a,col,fileName):
 #Input: list of dictionaries, key to sort by and output file name
 #Output: None
-
-	#Your code here:
-	pass
+	
+	tups_list = list()
+	new_list = sorted(a, key = lambda x: [x[col]])
+	for x in new_list:
+		new_tup = (x["First"], x["Last"], x["Email"])
+		tups_list.append(new_tup)
+	
+	
+	with open(fileName, "w") as f:
+		writer = csv.writer(f)
+		#writer.writerow(["First", "Last", "Email"])
+		writer.writerows(tups_list)
+	
+	return
+			
+		
+	
+		
 
 
 
@@ -143,7 +192,7 @@ def main():
 	print("Read in Test data and store as a list of dictionaries")
 	data = getData('P1DataA.csv')
 	data2 = getData('P1DataB.csv')
-	total += test(type(data),type([]),35)
+	total += test(type(data),type([]),40)
 	print()
 	print("First student sorted by First name:")
 	total += test(mySort(data,'First'),'Abbot Le',15)
